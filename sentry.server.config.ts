c02141128995f1f4fab969f1406746af6e0f2c98
Sandeeb Adhikari <sa2734@njit.edu>
2024-10-13 09:08:39 -0400

    add sentry and example error page

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e0109082736b45bd2a97f849c824c3be@o4508115526287360.ingest.de.sentry.io/4508115534938192",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
