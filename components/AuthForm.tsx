75e1ab11f55a7ab0a8233421535e41ead9e2776a
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-07 09:51:45 -0500

    made some changes

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,7 +22,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, signIN, signUP } from "@/lib/actions/user.actions";
 import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -68,7 +68,7 @@ const AuthForm = ({ type }: { type: string }) => {
       }
 
       if (type === "sign-in") {
-        const response = await signIn({
+        const response = await signIN({
           email: data.email,
           password: data.password,
         });
